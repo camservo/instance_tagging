e349dc4a6bd4103176684edda9e0a0ef3623b21a
Cameron Jeffries <camservo@gmail.com>
2018-08-24 12:24:06 -0500

    simplified parse_csv with Text::CSV::Slurp module

--- parse_csv.pl
+++ parse_csv.pl
@@ -2,47 +2,32 @@
 
 use strict;
 use warnings;
-use Text::CSV;
+use Text::CSV::Slurp;
 
 # Should pass the file as argument.
 my $file = "test.csv";
+my $data = &parse_csv($file);
 
-# It would be nice to have this dereferenced.  I hate dealing with ref data.
-my @refData = &parse_csv($file);
-
-foreach my $instance (@refData) {
+foreach my $instance (@$data) {
     tag_instance($instance)
 }
 
 sub parse_csv {
     my $file = shift;
-    open my $fh, "<", $file or die "$file: $!";
-    my $csv = Text::CSV->new ({
-        binary => 1,
-        auto_diag => 1
-    });
-
-    $csv->column_names ($csv->getline ($fh)); # use header to reference data.
-
-    my @data;
-    while (my $row = $csv->getline_hr ($fh)) {
-        push @data, [$row->{Name}, $row->{desc}] # reference data by name in header
-    }
-    close $fh;
-
-    return(@data);
+    my $data = Text::CSV::Slurp->load(file => $file );
+    
+    return($data)
 }
 
 sub tag_instance {
     my $instance = shift;
 
-    my $name = $instance->[0];
-    my $desc = $instance->[1];
+    my $name = $instance->{Name};
+    my $desc = $instance->{desc};
+    print "name: $name\n";
+    print "desc: $desc\n";
 
     ##TODO: parse tag for existing values.
 
     ##TODO: create new tag that keeps existing values and writes new ones.
-
-    # print "Name: ", $name, ":\n";
-    # print "Desc: ", $desc, ":\n";
 }
