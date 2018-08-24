77f189496941b3e3ceefc56c17bb965ab8bee17e
Cameron Jeffries <camservo@gmail.com>
2018-08-24 10:28:06 -0500

    better way to load up the csv

---
+++ test.pl
@@ -0,0 +1,20 @@
+use Text::CSV::Slurp;
+# use strict;
+ 
+
+
+# my $slurp = Text::CSV::Slurp->new();
+
+# load data from CSV input
+ 
+$filename = "test.csv"; 
+my $data = Text::CSV::Slurp->load(file       => $filename );
+
+$, = "\n";
+print keys $data->[0];
+print $data->[0]{desc};
+# my $data = Text::CSV::Slurp->load(filehandle => $filehandle [,%options]);
+# my $data = Text::CSV::Slurp->load(string     => $string     [,%options]);
+ 
+# create a string of CSV from an array of hashes
+# my $csv  = Text::CSV::Slurp->create( input => \@array_of_hashes);
